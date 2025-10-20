fbd39c6b75178c6821290c86cf1318088fb1fe2b
yeni023 <yeeun3011@naver.com>
2025-10-20 12:17:13 +0900

    chore: vite 설정 수정
    - 절대 경로 alias 설정 (@/alias)
    - vite 관련 빌드 세팅 조정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import AppLayout from "../layouts/AppLayout";
-import HomePage from "../pages/HomePage";
-import FeaturesPage from "../pages/FeaturesPage";
-import GetStartedPage from "../pages/GetStartedPage";
+import PublicLayout from "../layouts/PublicLayout/PublicLayout";
+import LandingPage from "../pages/landing/LandingPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: "features", element: <FeaturesPage /> },
-      { path: "get-started", element: <GetStartedPage /> },
-    ],
+    element: <PublicLayout />,
+    children: [{ index: true, element: <LandingPage /> }],
   },
 ]);
+
+export default router;
