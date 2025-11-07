88223466b842e8d32e0409085966412d5b20f433
yeainlee <lyy923923@gmail.com>
2025-11-07 18:57:53 +0900

    :sparkles: 라우터 설정, axios 추가 설치

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import PublicLayout from "../layouts/PublicLayout/PublicLayout";
 import LandingPage from "../pages/landing/LandingPage";
+import AccommodationDetailPage from "../pages/accommodation/AccommodationDetailPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <PublicLayout />,
-    children: [{ index: true, element: <LandingPage /> }],
+    children: [
+        { index: true, element: <LandingPage /> },
+        { path : "accommodation/:id", element:<AccommodationDetailPage/>},
+    ],
   },
 ]);
 
