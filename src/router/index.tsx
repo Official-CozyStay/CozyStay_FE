5f39fd203a45dc9f33ce4051d1bb2f42143bc819
yeainlee <lyy923923@gmail.com>
2025-11-08 01:40:14 +0900

    :sparkles: router 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import PublicLayout from "../layouts/PublicLayout/PublicLayout";
 import LandingPage from "../pages/landing/LandingPage";
-import AccommodationDetailPage from "../pages/accommodation/AccommodationDetailPage";
+import AccommodationDetailPage from "../pages/accommodation/AccommodationDetailPage.tsx";
 
 export const router = createBrowserRouter([
   {
