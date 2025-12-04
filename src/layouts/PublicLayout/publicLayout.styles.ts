35f80300423ffc82d36ed7668415c657b73f35f3
yeni023 <yeeun3011@naver.com>
2025-12-04 22:06:33 +0900

    refactor: PublicLayout에서 사용되지 않는 styled-components 제거

--- src/layouts/PublicLayout/publicLayout.styles.ts
+++ src/layouts/PublicLayout/publicLayout.styles.ts
@@ -1,51 +1,9 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-`;
-
-export const Header = styled.header`
-  height: ${({ theme }) => theme.spacing["4xl"]};
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 ${({ theme }) => theme.spacing.xl};
-  border-bottom: 1px solid ${({ theme }) => theme.colors.border.light};
-`;
-
-export const Brand = styled(Link)`
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-`;
-
-export const Logo = styled.img`
-  width: 105px;
-  height: 70px;
-  margin-right: -18px;
-`;
-
-export const BrandTitle = styled.h1`
-  font-family: "Pacifico";
-  font-size: 26px;
-  font-weight: 400;
-  color: ${({ theme }) => theme.colors.primary.main};
-`;
-
-export const Nav = styled.nav`
-  display: flex;
-  gap: ${({ theme }) => theme.spacing.xl};
-  a {
-    text-decoration: none;
-    color: ${({ theme }) => theme.colors.primary.main};
-    font-weight: 500;
-    &:hover {
-      text-decoration: underline;
-    }
-  }
 `;
 
 export const Main = styled.main`
