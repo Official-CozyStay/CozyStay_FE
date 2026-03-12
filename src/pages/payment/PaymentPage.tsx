3ed8479ec56fc95019b909fbcec33fcb869ad558
yeainlee <lyy923923@gmail.com>
2026-03-12 17:19:04 +0900

    chore: import 경로에서 .ts 확장자 제거

--- src/pages/payment/PaymentPage.tsx
+++ src/pages/payment/PaymentPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import axios from "axios";
 
-import { useAccommodationStore } from "@/store/accommodationStore.ts";
+import { useAccommodationStore } from "@/store/accommodationStore";
 import { nightsBetween, calcTotal } from "../../utils/price";
 
 import BookingSummaryCard from "./components/BookngSummaryCard";
