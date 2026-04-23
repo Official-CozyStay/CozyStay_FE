c5663ea19694c722eb3615f853dfc85d936b3475
yeainlee <lyy923923@gmail.com>
2026-04-23 21:03:16 +0900

    fix: 프로필 예약 목록에서 전체 예약이 보이도록 수정

--- src/pages/profile/sections/PastTripsSection.tsx
+++ src/pages/profile/sections/PastTripsSection.tsx
@@ -183,7 +183,7 @@ const PastTripsSection = () => {
         setLoading(true);
 
         // 게스트의 내가 예약한 목록 조회
-        const gBookings = await fetchUserBookings('CONFIRMED');
+        const gBookings = await fetchUserBookings();
         setGuestBookings(gBookings || []);
 
         // 내가 예약한 숙소의 이름 정보를 가져오기 위해 숙소 디테일 조회 (ReviewPage 참조)
