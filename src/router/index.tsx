350385e73ea7f08d49e5557e3c3b53fa1733d7a1
mojiri00 <moguricamera@gmail.com>
2026-06-02 21:03:30 +0900

    refactor(router): 중첩 라우팅을 활용한 RequireAuth 코드 개선
    - 인증이 필요한 페이지들을 <Outlet />을 사용하여 하나의 RequireAuth 라우트로 그룹화하여 코드 중복 제거

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 import PublicLayout from '../layouts/PublicLayout/PublicLayout';
 import LandingPage from '../pages/landing/LandingPage';
 import MainPage from '../pages/main/MainPage';
@@ -26,113 +26,41 @@ export const router = createBrowserRouter([
       { index: true, element: <MainPage /> },
       { path: 'landing', element: <LandingPage /> },
       { path: 'accommodation/:id', element: <AccommodationDetailPage /> },
-      {
-        path: 'payment/:id',
-        element: (
-          <RequireAuth>
-            <PaymentPage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'payment/retry/:id',
-        element: (
-          <RequireAuth>
-            <PaymentPage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'payment/success',
-        element: (
-          <RequireAuth>
-            <PaymentSuccessPage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'payment/fail',
-        element: (
-          <RequireAuth>
-            <PaymentFailPage />
-          </RequireAuth>
-        ),
-      },
       { path: 'search', element: <SearchPage /> },
-      {
-        path: 'wishlist',
-        element: (
-          <RequireAuth>
-            <WishlistPage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'wishlist/:favoriteId',
-        element: (
-          <RequireAuth>
-            <WishlistDetailPage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'messages',
-        element: (
-          <RequireAuth>
-            <MessagePage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'account',
-        element: (
-          <RequireAuth>
-            <AccountPage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'profile',
-        element: (
-          <RequireAuth>
-            <ProfilePage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'profile/edit',
-        element: (
-          <RequireAuth>
-            <ProfileEditPage />
-          </RequireAuth>
-        ),
-      },
-      {
-        path: 'profile/reviews',
-        element: (
-          <RequireAuth>
-            <ProfilePage />
-          </RequireAuth>
-        ),
-      },
       { path: 'users/:id', element: <UserPage /> },
+      {
+        element: (
+          <RequireAuth>
+            <Outlet />
+          </RequireAuth>
+        ),
+        children: [
+          { path: 'payment/:id', element: <PaymentPage /> },
+          { path: 'payment/retry/:id', element: <PaymentPage /> },
+          { path: 'payment/success', element: <PaymentSuccessPage /> },
+          { path: 'payment/fail', element: <PaymentFailPage /> },
+          { path: 'wishlist', element: <WishlistPage /> },
+          { path: 'wishlist/:favoriteId', element: <WishlistDetailPage /> },
+          { path: 'messages', element: <MessagePage /> },
+          { path: 'account', element: <AccountPage /> },
+          { path: 'profile', element: <ProfilePage /> },
+          { path: 'profile/edit', element: <ProfileEditPage /> },
+          { path: 'profile/reviews', element: <ProfilePage /> },
+        ],
+      },
     ],
   },
   {
     path: '/hosting',
     element: (
       <RequireAuth>
-        <HostingPage />
+        <Outlet />
       </RequireAuth>
     ),
-  },
-  {
-    path: '/hosting/become-a-host',
-    element: (
-      <RequireAuth>
-        <BecomeHostPage />
-      </RequireAuth>
-    ),
+    children: [
+      { index: true, element: <HostingPage /> },
+      { path: 'become-a-host', element: <BecomeHostPage /> },
+    ],
   },
 ]);
 
